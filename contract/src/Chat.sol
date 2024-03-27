4fc983ab74e58415406857dd06d91e2e8751005c
PhantomOz <faniogor@gmail.com>
2024-03-27 22:49:19 +0100

    This adds the send Message function and other util function

--- contract/src/Chat.sol
+++ contract/src/Chat.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+error ChatSystem__SenderNotEnsOwner();
+
 /// @title A simple chatting systme
 /// @author @SuperDevFavour
 /// @notice A simple chat system for sending messages
@@ -9,18 +13,60 @@ contract ChatSystem {
     //Only Users with an ens can send message
     //app should be able to get all messages
     //chat system should be able to register or login
+    using Strings for uint256;
     address private immutable i_ensAddress; //ensAddress
-    mapping(string => mapping(string => Message[])) private s_ensToEnsMessages; //holds all messages sent to a user;
+    mapping(string => mapping(string => uint256)) private s_ensToEnsCoversionId; //holds the Ids message a user;
+    mapping(uint256 => Message[]) private s_conversationIdToMessages; //holds the messages for a conversation Id;
+    uint256 public conversationId = 1;
 
     // The Message Data type
     struct Message {
         string author;
+        string receiver;
         string message;
         string imageUrl;
-        uint256 createdAt;
+        string createdAt;
     }
 
     constructor(address _ensAddress) {
         i_ensAddress = _ensAddress;
+    }
+
+    function hasEns(string calldata _ensName) public returns (bool isUser) {
+        //carry Out the check to see to see
+    }
+
+    function _getConversationId(
+        string calldata _sender,
+        string calldata _receiver
+    ) internal view returns (uint256) {
+        return
+            s_ensToEnsCoversionId[_sender][_receiver] > 0
+                ? s_ensToEnsCoversionId[_sender][_receiver]
+                : s_ensToEnsCoversionId[_receiver][_sender];
+    }
+
+    function sendMessage(
+        string calldata _author,
+        string calldata _receiver,
+        string memory _message,
+        string memory _imageUrl
+    ) external {
+        if (!hasEns(_author)) {
+            revert ChatSystem__SenderNotEnsOwner();
+        }
+        uint256 _time = block.timestamp;
+
+        if (_getConversationId(_author, _receiver) <= 0) {
+            s_ensToEnsCoversionId[_author][_receiver] = conversationId;
+            conversationId += 1;
+        }
+
+        Message[] storage _messages = s_conversationIdToMessages[
+            _getConversationId(_author, _receiver)
+        ];
+        _messages.push(
+            Message(_author, _receiver, _message, _imageUrl, _time.toString())
+        );
     }
 }
