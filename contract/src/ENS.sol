5abd31fd60fa3214aa2f5d1927da88152fd8c796
PhantomOz <faniogor@gmail.com>
2024-03-27 23:26:19 +0100

    This adds the Ens Contract and registerEnsName

--- contract/src/ENS.sol
+++ contract/src/ENS.sol
@@ -1,4 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract ENS {}
+error ENS__NameAlreadyTaken();
+
+/// @title Ens Domain
+/// @author @SuperDevFavour
+/// @notice An Ens Domain contract
+contract ENS {
+    mapping(string => ENSDetails) public s_ensToDetails;
+
+    event ENSRegistered(address indexed _owner, string indexed _ensName);
+
+    struct ENSDetails {
+        address owner;
+        string imageURI;
+    }
+
+    function registerEnsName(
+        string memory _ensName,
+        string memory _imageURI
+    ) public {
+        if (s_ensToDetails[_ensName].owner != address(0)) {
+            revert ENS__NameAlreadyTaken();
+        }
+
+        s_ensToDetails[_ensName] = ENSDetails(msg.sender, _imageURI);
+
+        emit ENSRegistered(msg.sender, _ensName);
+    }
+}
